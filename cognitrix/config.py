e2c781af63a5c9c87bdab262f8d425f155a70bdf
theonlyamos <theonlyamos@gmail.com>
2024-06-06 15:06:15 +0000

    version update

--- cognitrix/config.py
+++ cognitrix/config.py
@@ -3,7 +3,7 @@ import asyncio
 import aiofiles
 import json
 
-VERSION = '0.2.3'
+VERSION = '0.2.4'
 SPIRAL_WORKDIR = Path('~').expanduser() / '.cognitrix'
 
 AGENTS_FILE = SPIRAL_WORKDIR / 'agents.json'
